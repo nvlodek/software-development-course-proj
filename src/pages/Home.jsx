01d6c42e7f80c225ff2f57c6f023a1a37e038bf5
navynd <navynd@umsl.edu>
2026-03-25 00:42:52 -0500

    FAQ section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from '../components/sections/Hero'
+import FAQ from '../components/sections/FAQ'
 
 function Home () {
     return (
         <div>
             <Hero />
+            <FAQ />
         </div>
     );
-};
+}
 
 export default Home;
