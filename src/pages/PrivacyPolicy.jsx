cce992b05632dc0f3bd829201d3b47b358127b72
navynd <navynd@umsl.edu>
2026-05-03 21:08:33 -0500

    policy page is done

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -460,7 +460,7 @@ function PrivacyPolicy() {
                 </p>
                 <div className="policy-buttons">
                     <button className="btn-primary">
-                        <a href="/terms" className="btn-link" target="blank" rel="noreferrer">
+                        <a href="/terms" className="btn-link" target="_blank" rel="noreferrer">
                             Read: Terms and Conditions </a>
                     </button>
                     <button className="btn-primary">
